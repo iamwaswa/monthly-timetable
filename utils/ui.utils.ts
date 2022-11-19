5803b36d052d50afe5ef848a373dda7763193662
Waswa Olunga <olungaw@gmail.com>
2022-11-18 21:22:38 -0800

    refactor: remove unused function

--- utils/ui.utils.ts
+++ utils/ui.utils.ts
@@ -1,15 +1,6 @@
 import { daysOfTheWeek, monthsOfTheYear } from "~/constants";
 import type { DayOfTheWeek, DayTasks, MonthOfTheYear, Task } from "~/types";
 
-function getScrollbarWidth() {
-  let scrollDiv = document.createElement("div");
-  scrollDiv.className = "scrollbar-measure";
-  document.body.appendChild(scrollDiv);
-  const scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-  document.body.removeChild(scrollDiv);
-  return scrollbarWidth;
-}
-
 export function createDayTasks(): Record<DayOfTheWeek, Task> {
   return daysOfTheWeek.reduce(
     (dayTasks, dayOfTheWeek) => ({
